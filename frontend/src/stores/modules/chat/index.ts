bfdbcc8b39bc38363ec3957f078d34ce745d9e9d
shengan.floating@gmail.com <>
2023-06-14 12:31:05 +0800

    删除所有文件中的https://sydney.vcanbb.chat

--- frontend/src/stores/modules/chat/index.ts
+++ frontend/src/stores/modules/chat/index.ts
@@ -28,7 +28,7 @@ export const useChatStore = defineStore(
         label: 'Bing 官方',
       },
       {
-        baseUrl: 'https://sydney.vcanbb.chat',
+        baseUrl: '',
         label: 'Cloudflare',
       },
       {
